2fa3ef8cd64a23451ad9c8c0f628659504500a8c
KartheekGurram21 <techloom.web3@gmail.com>
2025-08-07 20:40:56 +0530

    array to hexadecimal

--- convertToBinary.js
+++ convertToBinary.js
@@ -1,4 +1,17 @@
-let publicKey = "Ehjdbchjfbirc384ud34dbdj34idj83hnx89x3h98r3hci4";
+let publicKey = "Ehjdbchjfbirc384ud34dbdj34idj83hnx89x3h98r3hci4";// this is not ascii it's in base 58 format
 
 const binaryRepresentation = new TextEncoder().encode(publicKey); // it returns a uint array coz uint array takes less space over native arrays
 console.log(binaryRepresentation);
+
+function arrayToHex(byteArray) {
+  let hexString = '';
+  for(let i=0;i<byteArray.length;i++) {
+    hexString += byteArray[i].toString(16).padStart(2,'0');
+  }
+  return hexString;
+}
+
+const str = "hello";
+const byteArray = new TextEncoder().encode(str);
+const hexString = arrayToHex(byteArray);
+console.log(hexString);
